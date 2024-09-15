17f23b497da0ca0d6059c9a9277c7b94dde5583c
Harry Sharma <harrysharma1066@gmail.com>
2024-09-15 09:16:07 +0100

    feat: remove signal flags

--- text.c
+++ text.c
@@ -1,33 +1,48 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 struct termios originTermios;
 
+void killed(const char *s){
+	perror(s);
+	exit(1);
+}
+
+
 void rawModeDisabled(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &originTermios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &originTermios) == -1) killed("tcsetattr");
 }
 
 void rawModeEnabled(){
-	tcgetattr(STDIN_FILENO, &originTermios);
+	if (tcgetattr(STDIN_FILENO, &originTermios) == -1) killed("tcgetattr");
 	atexit(rawModeDisabled);
 	struct termios raw = originTermios;
-	raw.c_lflag &= ~(ECHO | ICANON);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) killed("tcsetattr");
 }
 
 int main()
 {
 	rawModeEnabled();
 	char c;
-	while(read(STDIN_FILENO,&c,1)==1 && c != 'q'){
-		if (iscntrl(c)){
-			printf("%d\n", c);
+	while (1) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) killed("read");
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
 		} else {
-			printf("%d ('%c')\n", c, c);
+			 printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 	return 0;
 }
