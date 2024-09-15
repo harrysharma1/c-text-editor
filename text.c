ffcdcd19eb4baeb55ed986ff57aeaadd74903fab
Harry Sharma <harrysharma1066@gmail.com>
2024-09-15 13:24:10 +0100

    TODO: Fix cursor pos

--- text.c
+++ text.c
@@ -5,16 +5,20 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 /* Definition */
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* Data  */
 struct editor_config {
+	int screen_rows;
+	int screen_columns;
 	struct termios origin_termios;
 };
 
 struct editor_config E;
+
 /* Terminal  */
 void killed(const char *s){
 	perror(s);
@@ -50,6 +54,41 @@ char editor_read_key(){
 	return c;
 }
 
+int get_cursor_position(int *rows, int *columns){
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\z1b[6n",4) != 4) return -1;
+
+	printf("\r\n");
+	char c;
+	while (i < sizeof(buf)-1 {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if buf[i] == 'R') break;
+		i++;
+	}
+
+	buf[i] = '\0';
+
+	printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+	editor_read_key()
+	return -1;
+}
+
+int get_window_size(int *rows, int *columns){
+	struct winsize ws;
+	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;	
+		return get_cursor_position(rows,columns);
+		return -1;
+	}else{
+		*columns = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
 /* Input  */
 void editor_keypress_process(){
 	char c = editor_read_key();
@@ -66,7 +105,7 @@ void editor_keypress_process(){
 /* Output  */
 void editor_draw_rows(){
 	int i;
-	for (i=0;i<24;i++){
+	for (i=0;i<E.screen_rows;i++){
 		write(STDOUT_FILENO,">\r\n",3);
 	}
 }
@@ -83,9 +122,13 @@ void editor_refresh_screen(){
 
 
 /* Init */
-int main()
-{
+void init_editor(){
+	if (get_window_size(&E.screen_rows, &E.screen_columns) == -1) killed("get_window_size");
+}
+
+int main(){
 	raw_mode_enabled();
+	init_editor();
 	char c;
 	while (1) {
 		editor_refresh_screen();
