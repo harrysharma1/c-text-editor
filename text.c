ec4070c59dfd655975fa98da00898576a7e7639c
Harry Sharma <harrysharma1066@gmail.com>
2024-09-15 11:40:33 +0100

    feat: add ctrl+q to exit program and keypress recognitions

--- text.c
+++ text.c
@@ -1,3 +1,4 @@
+/* Include */
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
@@ -5,21 +6,26 @@
 #include <termios.h>
 #include <unistd.h>
 
+/* Definition */
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/* Data  */
 struct termios originTermios;
 
+/* Terminal  */
 void killed(const char *s){
 	perror(s);
 	exit(1);
 }
 
 
-void rawModeDisabled(){
+void raw_mode_disabled(){
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &originTermios) == -1) killed("tcsetattr");
 }
 
-void rawModeEnabled(){
+void raw_mode_enabled(){
 	if (tcgetattr(STDIN_FILENO, &originTermios) == -1) killed("tcgetattr");
-	atexit(rawModeDisabled);
+	atexit(raw_mode_disabled);
 	struct termios raw = originTermios;
 	raw.c_oflag &= ~(OPOST);
 	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
@@ -30,11 +36,34 @@ void rawModeEnabled(){
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) killed("tcsetattr");
 }
 
+
+char editor_read_key() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c,1)) != 1){
+		if (nread == -1 && errno != EAGAIN) killed("read");
+	}
+
+	return c;
+}
+
+void editor_keypress_process(){
+	char c = editor_read_key();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
+/* Init */
 int main()
 {
-	rawModeEnabled();
+	raw_mode_enabled();
 	char c;
 	while (1) {
+		editor_keypress_process();
 		char c = '\0';
 		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) killed("read");
 		if (iscntrl(c)) {
@@ -42,7 +71,7 @@ int main()
 		} else {
 			 printf("%d ('%c')\r\n", c, c);
 		}
-		if (c == 'q') break;
+		if (c == CTRL_KEY('q')) break;
 	}
 	return 0;
 }
